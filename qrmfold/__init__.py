95ad34eca01a28d2050aad8d7118faba6d01a84c
Tim Chan <82181640+timchan0@users.noreply.github.com>
2026-01-22 17:02:42 +0900

    Clean init

--- qrmfold/__init__.py
+++ qrmfold/__init__.py
@@ -1,12 +1,2 @@
 from qrmfold import logical_index_to_subset_maps
 from qrmfold.quantum_reed_muller import QuantumReedMuller
-
-
-if __name__ == "__main__":
-    m = 4
-    qrm = QuantumReedMuller(
-        m,
-        logical_index_to_subset=logical_index_to_subset_maps.alternative(m, start_index=1),
-    )
-    pairs = [(1, 2)]
-    physical_circuit = qrm.automorphism_product(pairs)
